aa2778c74db93b7bedfbb9d3b84801531a6d53a1
RKs <riddhik84@gmail.com>
2020-05-29 15:42:43 +0530

    Solution

---
+++ moderate.problems/WordFreuencies.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+
+public class WordFreuencies {
+  public static void main(String[] args) {
+	  String[] book = {"abc" , "bcd", "test", "tess", "abcd", "", "  ", "aaaa", "aaaa"};
+	  String[] searchWords = {"", "a", "abc", "abcc", "aaaa"};
+	  HashMap<String, Integer>() wordMap = new HashMap<>();
+	  for (String s : searchWords){
+		  if(wordMap.containsKey(s)){
+			  System.out.println("Word count = " + wordMap.get(s));
+		  } else {
+			  int count = getWordCount(s);
+			  System.out.println("Word count = " + count);
+			  wordMap.put(s, count);
+		  }
+	  }
+	}
+	
+	public static int getWordCount(String[] book, String searchWord){
+		int counter = 0;
+		for(int i = 0; i < book.length; i++){
+			if(book[i].equals(searchWord)){
+				counter++;
+			}
+		}
+		return counter;
+	}
+}
